6de6bdd9b06588787618f44ed9d1e8b10b2b974c
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-05-30 12:53:57 -0500

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import Flashcards from "./pages/Flashcards";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <Flashcards />
     </div>
   );
