2b1c5a08f34b6c47389ed1ea541e94421d43de33
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-05-30 14:17:59 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import logo from "./logo.svg";
 import "./App.css";
 import Flashcards from "./pages/Flashcards";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Navbar } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <Flashcards />
+      <Navbar bg="info" variant="dark" sticky="top">
+        <Navbar.Brand>
+          <img src={logo} />
+          {""}
+          studious
+        </Navbar.Brand>
+      </Navbar>
+      <div className="content">
+        <Flashcards />
+      </div>
     </div>
   );
 }
