e1a9e9df96df30649d657d89abbba47a46f90a36
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-05-30 15:36:38 -0500

    navbar layout

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import "./App.css";
 import Flashcards from "./pages/Flashcards";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { NavDropdown } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="info" variant="dark" sticky="top">
+      <Navbar bg="info" variant="dark" sticky="top" expand="md">
         <Navbar.Brand>
-          <img src={logo} />
-          {""}
-          studious
+          <img src={logo} width="40px" height="40px" /> studious
         </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav>
+            <Nav.Link className="link">Collections</Nav.Link>
+            <Nav.Link className="link">Sets</Nav.Link>
+          </Nav>
+          <Nav className="link userNav">
+            <NavDropdown title="jtrevz" className="user">
+              <NavDropdown.Item>Account</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item>Sign out</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <div className="content">
         <Flashcards />
