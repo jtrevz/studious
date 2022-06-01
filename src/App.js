0f85d3ff293bcfffb708c08aa8a0617e86f9a6c0
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-05-31 21:05:51 -0500

    navbar in separate component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
+import React from "react";
 import "./App.css";
 import Flashcards from "./pages/Flashcards";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import { NavDropdown } from "react-bootstrap";
-import { BsHourglassSplit } from "react-icons/bs";
-import { BsLightbulbFill } from "react-icons/bs";
-import { BsLightbulb } from "react-icons/bs";
-import { BsPerson } from "react-icons/bs";
+import NavBar from "./components/NavBar";
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="myCustom" variant="dark" sticky="top" expand="md">
-        <Navbar.Brand>
-          <BsHourglassSplit className="logo" /> studious
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse>
-          <Nav>
-            <Nav.Link>Collections</Nav.Link>
-            <Nav.Link>Sets</Nav.Link>
-          </Nav>
-          <Nav className="user">
-            {/* <div className="pic">
-              <BsPerson classname="userPFP" />
-            </div> */}
-            <NavDropdown title="jtrevz" align="end">
-              <NavDropdown.Item>Account</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item>Sign out</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <NavBar />
       <div className="content">
         <Flashcards />
       </div>
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./styles.css";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { NavDropdown } from "react-bootstrap";
+import { BsHourglassSplit } from "react-icons/bs";
+import { BsLightbulbFill } from "react-icons/bs";
+import { BsLightbulb } from "react-icons/bs";
+import { BsPerson } from "react-icons/bs";
+
+export default function NavBar() {
+  return (
+    <Navbar bg="myCustom" variant="dark" sticky="top" expand="md">
+      <Navbar.Brand>
+        <BsHourglassSplit className="logo" /> studious
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Nav>
+          <Nav.Link>Collections</Nav.Link>
+          <Nav.Link>Sets</Nav.Link>
+        </Nav>
+        <Nav className="user">
+          {/* <div className="pic">
+              <BsPerson classname="userPFP" />
+            </div> */}
+          <NavDropdown title="jtrevz" align="end">
+            <NavDropdown.Item>Account</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item>Sign out</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
