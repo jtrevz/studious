f0d3386b1375991b76f8cd4b491393b37748e578
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-06-06 15:05:50 -0500

    adding study page

--- src/components/Flashcard/index.js
+++ src/components/Flashcard/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
+import "./styles.css";
 
 export default function Flashcard({ card }) {
   const [flip, setFlip] = useState(false);
