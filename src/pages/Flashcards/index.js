2fef59cdeea6ec57209baca51193dbf277b94dc6
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-06-06 16:03:42 -0500

    click through cards

--- src/pages/Flashcards/index.js
+++ src/pages/Flashcards/index.js
@@ -1,25 +1,57 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import Set from "../../components/Set";
+import Flashcard from "../../components/Flashcard";
+import {
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
+} from "react-icons/bs";
+import "./styles.css";
 
 export default function Flashcards() {
   const [cards, setCards] = useState(sampleInfo);
+  const [currentCard, setCurrentCard] = useState(0);
 
-  return <Set cards={cards}></Set>;
+  const next = () => {
+    if (currentCard === cards.length - 1) {
+      return setCurrentCard(0);
+    }
+    return setCurrentCard(currentCard + 1);
+  };
+
+  const last = () => {
+    if (currentCard === 0) {
+      return setCurrentCard(cards.length - 1);
+    }
+    return setCurrentCard(currentCard - 1);
+  };
+
+  // useEffect(() => {});
+
+  return (
+    <div>
+      <Flashcard card={cards[currentCard]} key={currentCard} />
+      <div>
+        <BsFillArrowLeftCircleFill className="arrows" onClick={last} />
+        <BsFillArrowRightCircleFill className="arrows" onClick={next} />
+        <p>{`${currentCard + 1}/${cards.length}`}</p>
+      </div>
+    </div>
+  );
 }
 
 const sampleInfo = [
   {
-    id: 1,
+    id: 0,
     front: "studium",
     back: "to study or pursuit",
   },
   {
-    id: 2,
+    id: 1,
     front: "amicus",
     back: "friend, companion",
   },
   {
-    id: 3,
+    id: 2,
     front: "pater",
     back: "father",
   },
