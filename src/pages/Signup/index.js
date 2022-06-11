a8279f886f8d573f9fdd11ca6e5602d3439eabdf
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-06-11 17:31:17 -0500

    signup done

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -12,7 +12,22 @@ export default function Signup() {
           <BsHourglassSplit size={80} color="white" />
         </div>
         <Form className="form col-lg-6 col-md-8 col-sm-10 col-xs-11">
-          <h3 className="bigBanner">Sign up here!</h3>
+          <h3 className="bigBanner">Welcome to studious!</h3>
+
+          <Row>
+            <Col>
+              <Form.Group className="mb-3">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control type="fName" placeholder="First Name" />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group className="mb-3">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control type="lName" placeholder="Last Name" />
+              </Form.Group>
+            </Col>
+          </Row>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
@@ -21,9 +36,8 @@ export default function Signup() {
             <Form.Label>Password</Form.Label>
             <Form.Control type="password" placeholder="Password" />
           </Form.Group>
-
           <h5 className="smallBanner">
-            Already have an account? <a href="./login">Log in here</a>
+            Already have an account? <a href="./login">Login here</a>
           </h5>
           <Button
             style={{ backgroundColor: "#376e6f", borderColor: "#376e6f" }}
