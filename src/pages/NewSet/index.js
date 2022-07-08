134ee5effba48a76d9f0de60cb3d299a358e2a0d
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-07-07 20:59:22 -0500

    initial new set layout

--- src/pages/NewSet/index.js
+++ src/pages/NewSet/index.js
@@ -1,8 +1,29 @@
 import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 import { useNewCardContext } from "./../../utils/NewCardContext";
 
 export default function NewSet() {
   const { currentSet } = useNewCardContext();
 
-  return <div>{currentSet}</div>;
+  return (
+    <div>
+      <Container fluid>
+        <Row>
+          <Col className="setContainer">
+            <h1 className="setName">{currentSet}</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col >
+            <div className="term col-sm-12 col-4">hello</div>
+            <div className="line"></div>
+            <div className="description col-sm-12  col-7">hello</div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
