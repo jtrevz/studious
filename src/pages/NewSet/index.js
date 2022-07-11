e9c439408237d802e0c449706d1106c209b4c3f7
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-07-11 12:29:52 -0500

    card setup

--- src/pages/NewSet/index.js
+++ src/pages/NewSet/index.js
@@ -4,22 +4,27 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import { useNewCardContext } from "./../../utils/NewCardContext";
+import "./styles.css";
 
 export default function NewSet() {
   const { currentSet } = useNewCardContext();
 
   return (
     <div>
-      <Container fluid>
+      <Container>
         <Row>
           <Col className="setContainer">
             <h1 className="setName">{currentSet}</h1>
           </Col>
         </Row>
-        <Row>
-          <Col >
-            <div className="term col-sm-12 col-4">hello</div>
+        <Row xs={12} className="cardContainer">
+          <Col sm={12} md={4}>
+            <div className="term">hello</div>
+          </Col>
+          <Col>
             <div className="line"></div>
+          </Col>
+          <Col sm={12} md={8}>
             <div className="description col-sm-12  col-7">hello</div>
           </Col>
         </Row>
