fa4860e9a7cd519a64e9303a5f33591dcde0cf89
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-08-11 21:09:40 -0500

    account page  layout

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -1,6 +1,31 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import stickFigure from "../../utils/pics/rihannastickfigure.jpeg";
 import "./styles.css";
 
 export default function Account() {
-  return <div>Account</div>;
+  return (
+    <div className="d-flex justify-content-center">
+      <Container className="accountCont mt-4">
+        <Row className="infoRow">
+          <Col
+            className="d-flex justify-content-center userPic userCard"
+            sm={4}
+          >
+            <div className="imageCont d-flex justify-content-center">
+              <img id="userPicture" src={stickFigure}></img>
+              <div className="userND">@<span >jtrevz</span></div>
+            </div>
+          </Col>
+          <Col sm={7} className="userInfo userCard">
+            <Row>name</Row>
+            <Row>username</Row>
+            <Row>password</Row>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
