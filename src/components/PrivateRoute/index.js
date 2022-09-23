bf8cc26fd42d1114b238d162ac26130f799bffd7
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-09-22 23:40:12 -0500

    private routes working

--- src/components/PrivateRoute/index.js
+++ src/components/PrivateRoute/index.js
@@ -6,9 +6,7 @@ import { Navigate, Outlet } from "react-router-dom";
 const PrivateRoute = () => {
   const currentUser = useAuthContext();
 
-  // If authorized, return an outlet that will render child elements
-  // If not, return element that will navigate to login page
-  return currentUser.email ? <Outlet /> : <Navigate to="/login" />;
+  return currentUser ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
