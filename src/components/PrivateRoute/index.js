ad0540faa3f3ba8645c9964e4f369a3d1f717617
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-09-28 15:08:27 -0500

    fixed private route?

--- src/components/PrivateRoute/index.js
+++ src/components/PrivateRoute/index.js
@@ -6,7 +6,7 @@ import { Navigate, Outlet } from "react-router-dom";
 const PrivateRoute = () => {
   const currentUser = useAuthContext();
 
-  return currentUser ? <Outlet /> : <Navigate to="/login" />;
+  return currentUser.email ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
