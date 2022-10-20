342c05d85ee7875850d2980ace8d8d93d5b7802a
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-10-19 21:52:52 -0500

    changed server files to public

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname, "build"));
+// app.use(express.static(__dirname, "build"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("public"));
+}
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  //   res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 app.listen(port, () => {
   console.log("Server is up!");
