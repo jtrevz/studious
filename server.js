f2ce538ea85e7ff7561f906d6f10dc04113a51a9
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-10-19 23:19:01 -0500

    build folder and var replaced

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const port = process.env.PORT || 3000;
 // app.use(express.static(__dirname, "build"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("public"));
+  app.use(express.static("build"));
 }
 
 app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "build", "index.html"));
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+  // res.sendFile(path.join(__dirname, "public/index.html"));
 });
 app.listen(port, () => {
   console.log("Server is up!");
